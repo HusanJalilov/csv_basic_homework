f979510ecd2fd179e587a6dc2c917ffb4715abb8
Hu$an <76938934+HusanJalilov@users.noreply.github.com>
2022-01-25 05:28:35 +0500

    TAsk 02

--- find_number_of_rows.py
+++ find_number_of_rows.py
@@ -6,6 +6,17 @@ def find_number_of_rows(data):
     Return:
         int: Number of rows.
     """
-    return
+    s=0
+    column_name=data.split()
+    
+    
+    
+    
+    return len(column_name)
+
+data = open('data.csv').read()
+print(find_number_of_rows(data))
+    
+  
 
 # Read the csv file
